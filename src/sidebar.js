f8db86b825eeed1b02e502e2ccb7ff1a06492c84
Samyog Lamsal <samyoglamsal@outlook.com>
2023-12-25 15:00:53 -0500

    Button disabled until download is finished

--- src/sidebar.js
+++ src/sidebar.js
@@ -7,17 +7,20 @@ async function onSubmit(event) {
     // Prevents the page from reloading after hitting submit -- caused many headaches
     event.preventDefault();
 
+    const artistField = document.getElementById("artist");
+    const titleField = document.getElementById("title");
+    const submitButton = document.getElementById("submit");
     const tabs = await browser.tabs.query({ currentWindow: true, active: true });
-    const artist = document.getElementById("artist").value;
-    const title = document.getElementById("title").value;
-    const date_added = (new Date()).toISOString().split("T")[0];
-    const url = tabs[0].url.split("&")[0];
+
+    artistField.disabled = true;
+    titleField.disabled = true;
+    submitButton.disabled = true;
 
     const body = {
-        artist: artist,
-        title: title,
-        date_added: date_added,
-        url: url
+        artist: artistField.value,
+        title: titleField.value,
+        date_added: new Date().toISOString().split("T")[0],
+        url: tabs[0].url.split("&")[0]
     };
 
     const response = await fetch("http://localhost:5000/download-song", {
@@ -29,4 +32,11 @@ async function onSubmit(event) {
     });
 
     console.log(response.json());
+
+    artistField.value = "";
+    titleField.value = "";
+
+    artistField.disabled = false;
+    titleField.disabled = false;
+    submitButton.disabled = false;
 }
